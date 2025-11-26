243d6dbac1c04459573893e81e34c6b858c86791
Ayush Dubey <ayushdubeyit@gmail.com>
2025-11-26 16:37:05 +0530

    first basic project of spring of game

---
+++ 01-spring/learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/game/ContraGame.java
@@ -0,0 +1,22 @@
+package com.in28minutes.learnspringframework.game;
+
+public class ContraGame implements GamingConsole{
+
+	public void up() {
+		System.out.println("up it");
+	}
+
+	public void down() {
+		System.out.println("Sit down here ");
+	}
+	
+	public void left() {
+		System.out.println("Go back to home");
+	}
+
+	public void right() {
+		System.out.println("Shooting a bullet");
+	}
+
+}
+//this is the functionality of contragame
---
+++ 01-spring/learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/game/GamerConsole.java
@@ -0,0 +1,9 @@
+package com.in28minutes.learnspringframework.game;
+
+public interface GamingConsole {
+	void up();
+	void down();
+	void left();
+	void right();
+}
+//this is the gaming console interface where dummy declaration of basic common feature of game is declared
