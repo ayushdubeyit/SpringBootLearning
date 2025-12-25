40ff960a89522f91250d851e3cad1358701707af
Ayush Dubey <ayushdubeyit@gmail.com>
2025-12-26 00:11:41 +0530

    added Basic spring boot Rest Api

---
+++ 02-Basic Rest Api/src/main/java/com/example/restapi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.restapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/health")
+    public String healthchecker(){
+        return "Application is running successfully";
+    }
+
+}
